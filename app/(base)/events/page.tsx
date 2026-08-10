08f949e9a91d00c1882376bc05fca84256324544
souvikofficial <souvikghosh6392@gmail.com>
2026-08-10 20:01:41 +0530

    Added card component

--- app/(base)/events/page.tsx
+++ app/(base)/events/page.tsx
@@ -1,12 +1,14 @@
+import { Card13 } from "@/components/flx/patterns/card/card-13";
+
 export default function EventsPage() {
     return (
-        <div className="min-h-dvh bg-background text-foreground">
+        <div className="min-h-dvh bg-background text-foreground lg:mx-20 md:mx-15 mx-5">
             <div className="mx-auto w-full max-w-7xl px-4 pb-8 pt-24 sm:px-6 lg:px-8">
                 <div className="space-y-1">
                     <h1 className="text-2xl font-semibold tracking-tight">
                         Events
                     </h1>
-
+                    <Card13 />
                     <p className="text-sm text-muted-foreground">
                         Discover upcoming events.
                     </p>
--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,16 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
